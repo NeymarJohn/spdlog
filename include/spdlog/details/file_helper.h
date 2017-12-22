b30df16f9eb1fd36010ea1c04529652ebc93c076
Sunriver <seriousman1994217winter@gmail.com>
2017-12-22 14:38:44 +0200

    report about unknown excepptions before rethrow

--- include/spdlog/details/file_helper.h
+++ include/spdlog/details/file_helper.h
@@ -118,18 +118,17 @@ public:
     //
     // "my_folder/.mylog" => ("my_folder/.mylog")
     // "my_folder/.mylog.txt" => ("my_folder/.mylog", ".txt")
+
     static std::tuple<filename_t, filename_t> split_by_extenstion(const filename_t& fname)
     {
         auto index = fname.rfind('.');
-        if (index != filename_t::npos && index != fname.size() - 1 &&index !=0 && fname[index - 1] != details::os::folder_sep) 
-        {
-            auto index2 = fname.find(details::os::folder_sep, index);
-            if (index2 == fname.npos) {
-                 return std::make_tuple(fname.substr(0, index), fname.substr(index));
-            }
-        }
-        return std::make_tuple(fname, std::string());
+        bool found_ext = index != filename_t::npos && index !=0 && fname[index - 1] != details::os::folder_sep;
+        if (found_ext)
+            return std::make_tuple(fname.substr(0, index), fname.substr(index));
+        else
+            return std::make_tuple(fname, filename_t());
     }
+
 private:
     FILE* _fd;
     filename_t _filename;
